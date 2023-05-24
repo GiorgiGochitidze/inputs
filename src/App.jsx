d4c2fad0eba36d160de33a2f266ecded200bb73c
Giorgi Gochitidze <giorgigochitidze555@gmail.com>
2023-05-24 08:23:25 +0400

    new file:   .eslintrc.cjs 	new file:   .gitignore 	new file:   index.html 	new file:   package-lock.json 	new file:   package.json 	new file:   public/vite.svg 	new file:   src/App.css 	new file:   src/App.jsx 	new file:   src/assets/react.svg 	new file:   src/index.css 	new file:   src/main.jsx 	new file:   vite.config.js

---
+++ src/App.jsx
@@ -0,0 +1,82 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [input1, setInput1] = useState('')
+  const [input2, setInput2] = useState('')
+  const [divs, setDivs] = useState([])
+  const [message, setMessage] = useState(false)
+
+  const handleCreateDiv = (e) => {
+    e.preventDefault();
+
+    if (input1 === '' || input2 === '') {
+      setMessage(true)
+      return; // Abort the function if inputs are empty
+    }
+
+    const newDiv = {
+      background: 'white',
+      input1,
+      input2
+    }
+
+    setDivs(divs => [...divs, newDiv])
+    setInput1('')
+    setInput2('')
+    setMessage(false)
+  }
+
+  const handleDeleteDiv = (index) => {
+    let newDivs = [...divs]
+    newDivs.splice(index, 1)
+    setDivs(newDivs)
+  }
+
+  return (
+    <div className="container">
+      <form className='inputs-container'>
+        <div>
+          <label>Name</label>
+          <input value={input1} type='text' id='name' placeholder='First Name' onChange={(e) => setInput1(e.target.value)} />
+
+          <label>Lastname</label>
+          <input value={input2} type='text' id='lastname' placeholder='Last Name' onChange={(e) => setInput2(e.target.value)} />
+
+          <button onClick={handleCreateDiv} className='submit-btn'>Submit</button>
+        </div>
+        {message && <h3 style={{ marginTop: '10px' }}>Please write your firstname and lastname</h3>}
+      </form>
+      <div style={{width: '100%', height: 'auto', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+        {divs.map((div, index) => (
+          <div
+            key={index}
+            style={{
+              width: '90%',
+              height: '70px',
+              background: div.background,
+              display: 'flex',
+              marginTop: '50px',
+              paddingInline: '20px',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              gap: '10px'
+            }}
+          >
+            <div style={{ display: 'flex' }}>
+              <h3>First Name:</h3>
+              <h3 style={{marginInline: '10px'}}>{div.input1};</h3>
+
+              <h3>Last Name:</h3>
+              <h3 style={{marginInline: '10px'}}>{div.input2};</h3>
+            </div>
+
+            <button onClick={() => handleDeleteDiv(index)} className='delete-cards'>X</button>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default App
